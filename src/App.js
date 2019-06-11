b6a4cc4173124c59a701d558995dd8a17e103c95
qihong1 <qihong1@lenovo.com>
2019-06-11 17:37:14 +0800

    asdf

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React, {
   Component
 } from 'react';
-import logo from './logo.svg';
 import './App.css';
 // import axios from 'axios';
-import {
-  Link,
-  IndexLink
-} from 'react-router';
+
 
 
 import moment from 'moment';
